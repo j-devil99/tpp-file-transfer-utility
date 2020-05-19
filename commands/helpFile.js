3b2a3a2caa9e41b7027bee8baf89d3e717d01e76
j-devil99 <jaidev.bhui@gmail.com>
2020-05-19 14:56:14 +0530

    added documentation in help module

--- commands/helpFile.js
+++ commands/helpFile.js
@@ -1,3 +1,44 @@
 module.exports.help = function help() {
-  console.log("help command is implemented");
+  let helpString = `
+    NAME: 
+
+      tpp - file transfer utility
+
+    USAGE:
+
+      node tpp [COMMAND]... [PATH ARGS(s)]... [MODE]...
+
+    COMMANDS:
+
+      view:
+
+        To view the complete structure of a directory.
+
+        TreeView (-t) :
+
+          node tpp view dirPath -t
+
+        FlatFileView (-f) :
+
+          node tpp view dirPath -f
+
+      treefy:
+
+        To convert the complete structure of a directory into a JSON file, 
+        and transfer the files to the destinantion path.
+
+          node tpp treefy srcPath destPath
+
+      untreefy:
+
+        To create the directory structure from a JSON file, and transfer 
+        the files.
+        
+          node tpp untreefy srcPath destPath.
+
+      help:
+
+        To view the documentation.
+  `;
+  console.log(helpString);
 };
