9cdc0855f515ef20fcb175f43fe6dd88b9a4f2cf
j-devil99 <jaidev.bhui@gmail.com>
2020-05-19 14:30:42 +0530

    integrated treefy module

--- commands/treefyFile.js
+++ commands/treefyFile.js
@@ -1,3 +1,28 @@
-module.exports.treefy = function treefy() {
-  console.log("treefy command is implemented");
+let fs = require("fs");
+let path = require("path");
+
+function treefyLogic(src, dest, node) {
+  if (node.isFile == true) {
+    let srcPath = path.join(src, node.newName);
+    let destPath = path.join(dest, node.oldName);
+    fs.copyFileSync(srcPath, destPath);
+    console.log(`File Copied from ${srcPath} to ${destPath}`);
+  } else {
+    let dirPath = path.join(dest, node.name);
+    if (!fs.existsSync(dirPath)) {
+      fs.mkdirSync(dirPath);
+    }
+    for (let i = 0; i < node.children.length; i++) {
+      let child = node.children[i];
+      let pPath = dirPath;
+      treefyLogic(src, pPath, child);
+    }
+  }
+}
+
+module.exports.treefy = function () {
+  let src = arguments[0],
+    dest = arguments[1];
+  let root = require(path.join(src, "metadata.json"));
+  treefyLogic(src, dest, root);
 };
--- tpp.js
+++ tpp.js
@@ -12,7 +12,7 @@ switch (cmd) {
     viewFile.view(input[1], input[2]);
     break;
   case "treefy":
-    treefyFile.treefy();
+    treefyFile.treefy(input[1], input[2]);
     break;
   case "untreefy":
     untreefyFile.untreefy(input[1], input[2]);
